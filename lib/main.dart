f243ef95831f9d1c18ee336c79b63ac9dcc6dd56
Manoj <manojkarthick1706@gmail.com>
2024-12-11 19:12:10 +0530

    Removed default body in application

--- lib/main.dart
+++ lib/main.dart
@@ -32,12 +32,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
   void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+    setState(() {});
   }
 
   @override
@@ -69,33 +65,31 @@ class _MyHomePageState extends State<MyHomePage> {
               )),
         ],
       ),
-      body: Center(
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: Column(children: [
+        Container(
+          margin: EdgeInsets.all(8),
+          child: Row(
+            children: [
+              Expanded(
+                  child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Search by Saloon'),
+              )),
+              Container(
+                margin: EdgeInsets.all(8),
+                child: Icon(
+                  Icons.location_on,
+                  size: 30,
+                ),
+              )
+            ],
+          ),
         ),
-      ),
+        Row(
+          children: [],
+        )
+      ]),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
