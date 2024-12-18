c2259ba805af593ca80e5eb3471e48c41b5ee6fa
Manoj <manojkarthick1706@gmail.com>
2024-12-18 09:11:19 +0530

    All three timers are working, including change in mode

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -14,58 +14,127 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
+class Mode {
+  final String key;
+  final String name;
+  final int time; // time in minutes
+
+  const Mode({
+    required this.key,
+    required this.name,
+    required this.time,
+  });
+}
+
 class _HomePageState extends State<HomePage> {
-  final Map<String, dynamic> _mode = const {
-    "Pomo": {
-      "key": "Pomo",
-      "name": "Pomorodo",
-      "time": 25,
-    },
-    "ShortBreak": {
-      "key": "ShortBreak",
-      "name": "Short Break",
-      "time": 5,
-    },
-    "LongBreak": {
-      "key": "LongBreak",
-      "name": "Long Break",
-      "time": 15,
-    }
+  static const Map<String, dynamic> _mode = const {
+    "Pomo": Mode(
+      key: "Pomo",
+      name: "Pomodoro",
+      time: 25,
+    ),
+    "ShortBreak": Mode(
+      key: "ShortBreak",
+      name: "Short Break",
+      time: 5,
+    ),
+    "LongBreak": Mode(
+      key: "LongBreak",
+      name: "Long Break",
+      time: 15,
+    ),
   };
 
   String activeMode = "Pomo";
 
   // Timer duration in seconds (25 minutes)
-  int _totalSeconds = 1 * 60;
+  int _remainingSeconds = _mode[modes.Pomo].time * 60;
 
   // Display time string
   String get _timeString {
-    int minutes = _totalSeconds ~/ 60;
-    int seconds = _totalSeconds % 60;
+    int minutes = _remainingSeconds ~/ 60;
+    int seconds = _remainingSeconds % 60;
     return "${minutes.toString().padLeft(2, '0')}:${seconds.toString().padLeft(2, '0')}";
   }
 
   Timer? _timer; // Timer instance
+  bool _isRunning = false;
+  bool _isPaused = false;
 
   void _startTimer() {
+    if (_isRunning && _isPaused) {
+      _resumeTimer();
+    } else {
+      setState(() {
+        _isRunning = true;
+        _isPaused = false;
+      });
+    }
     if (_timer != null) {
       _timer!.cancel();
     }
     _timer = Timer.periodic(Duration(seconds: 1), (timer) {
-      setState(() {
-        if (_totalSeconds > 0) {
-          _totalSeconds--;
-        } else {
-          timer.cancel(); // Stop timer when it reaches 0
-        }
-      });
+      if (_remainingSeconds > 0) {
+        setState(() {
+          _remainingSeconds--;
+        });
+      } else {
+        _stopTimer();
+      }
     });
   }
 
-  void _handleMode(String modeName) {
+  // Pause the Timer
+  void _pauseTimer() {
+    if (_timer != null) {
+      setState(() {
+        _isPaused = true;
+        _isRunning = false;
+      });
+      _timer!.cancel();
+    }
+  }
+
+  // Resume the Timer
+  void _resumeTimer() {
     setState(() {
-      this.activeMode = modeName;
-      print(this.activeMode);
+      _isRunning = true;
+      _isPaused = false;
+    });
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      if (_remainingSeconds > 0) {
+        setState(() {
+          _remainingSeconds--;
+        });
+      } else {
+        _stopTimer();
+      }
+    });
+  }
+
+  // Reset the Timer
+  void _resetTimer() {
+    setState(() {
+      _timer?.cancel();
+      _remainingSeconds = _mode[activeMode].time * 60;
+      _isRunning = false;
+      _isPaused = false;
+    });
+  }
+
+  // Stop Timer
+  void _stopTimer() {
+    _timer?.cancel();
+    setState(() {
+      _isRunning = false;
+      _isPaused = false;
+    });
+  }
+
+  void _handleMode(Mode mode) {
+    setState(() {
+      this.activeMode = mode.key;
+      _resetTimer();
     });
   }
 
@@ -131,28 +200,80 @@ class _HomePageState extends State<HomePage> {
                     height: 1.0,
                   ),
                 ),
-                ElevatedButton(
-                  onPressed: _startTimer,
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Theme.of(context).colorScheme.onSurface,
-                    foregroundColor: Colors.white,
-                    // Text color
-                    padding: EdgeInsets.symmetric(horizontal: 24, vertical: 2),
-                    // Button size
-                    shape: RoundedRectangleBorder(
-                      borderRadius:
-                          BorderRadius.circular(8), // Adjust border radius here
-                    ),
-                    elevation: 2,
-                  ), // Button shadow),
-                  child: Text(
-                    'START',
-                    style: GoogleFonts.dongle(
-                      fontSize: 40,
-                      color: Theme.of(context).colorScheme.secondary,
-                    ),
-                  ),
-                ),
+                (_isRunning || _isPaused)
+                    ? Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          ElevatedButton(
+                            onPressed: _isPaused ? _resumeTimer : _pauseTimer,
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor:
+                                  Theme.of(context).colorScheme.onSurface,
+                              foregroundColor: Colors.white,
+                              // Text color
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 24, vertical: 2),
+                              // Button size
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(
+                                    8), // Adjust border radius here
+                              ),
+                              elevation: 2,
+                            ), // Button shadow),
+                            child: Text(
+                              _isPaused ? 'RESUME' : 'PAUSE',
+                              style: GoogleFonts.dongle(
+                                fontSize: 40,
+                                color: Theme.of(context).colorScheme.secondary,
+                              ),
+                            ),
+                          ),
+                          SizedBox(width: 6),
+                          ElevatedButton(
+                            onPressed: _resetTimer,
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor:
+                                  Theme.of(context).colorScheme.primary,
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 18, vertical: 18),
+                              // Button size
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(
+                                    8), // Adjust border radius here
+                              ),
+                              elevation: 2,
+                            ), // Button shadow),
+                            child: Icon(
+                              Icons.restart_alt,
+                              color: Theme.of(context).colorScheme.onPrimary,
+                            ),
+                          )
+                        ],
+                      )
+                    : ElevatedButton(
+                        onPressed: _startTimer,
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor:
+                              Theme.of(context).colorScheme.onSurface,
+                          foregroundColor: Colors.white,
+                          // Text color
+                          padding:
+                              EdgeInsets.symmetric(horizontal: 24, vertical: 2),
+                          // Button size
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(
+                                8), // Adjust border radius here
+                          ),
+                          elevation: 2,
+                        ), // Button shadow),
+                        child: Text(
+                          'START',
+                          style: GoogleFonts.dongle(
+                            fontSize: 40,
+                            color: Theme.of(context).colorScheme.secondary,
+                          ),
+                        ),
+                      ),
               ],
             ),
           ),
--- lib/widgets.dart/modes.dart
+++ lib/widgets.dart/modes.dart
@@ -1,29 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:learn_flutter/pages/home_page.dart';
 
 class Modes extends StatelessWidget {
   const Modes(this.mode, this.isActive, this.handleMode, {super.key});
 
-  final Map<String, dynamic> mode;
+  final Mode mode;
   final bool isActive;
-  final Function(String) handleMode;
+  final Function(Mode) handleMode;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(8),
-      margin: EdgeInsets.symmetric(horizontal: 8),
-      decoration: BoxDecoration(
-        color: isActive
-            ? Theme.of(context).colorScheme.inversePrimary
-            : Theme.of(context).colorScheme.secondary,
-        borderRadius: BorderRadius.circular(8),
-      ),
-      child: InkWell(
-        onTap: () {
-          handleMode(mode["key"]!);
-        },
+    return InkWell(
+      onTap: () {
+        handleMode(mode);
+      },
+      child: Container(
+        padding: EdgeInsets.all(8),
+        margin: EdgeInsets.symmetric(horizontal: 8),
+        decoration: BoxDecoration(
+          color: isActive
+              ? Theme.of(context).colorScheme.inversePrimary
+              : Theme.of(context).colorScheme.secondary,
+          borderRadius: BorderRadius.circular(8),
+        ),
         child: Text(
-          mode["name"]!,
+          mode.name,
           style: TextStyle(
             color: isActive
                 ? Theme.of(context).colorScheme.primary
